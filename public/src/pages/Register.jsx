fdaaeec5853168e22e1aacdf42ffa53c0d021c82
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-06-19 01:36:56 +0530

    Update Register.jsx

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -65,7 +65,7 @@ export default function Register() {
 		event.preventDefault();
 		if (handleValidation()) {
 			const { email, username, password } = values;
-			const { data } = await axios.post('https://connectify-pearl.vercel.app/registerRoute' ,registerRoute, {
+			const { data } = await axios.post('https://connectify-pearl.vercel.app/Register' ,registerRoute, {
 				username,
 				email,
 				password,
